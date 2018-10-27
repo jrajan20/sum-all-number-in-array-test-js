10b454a20ef5e102673712bcd3356fdd2518d774
jrajan20 <jai_20j@hotmail.com>
2018-10-27 02:27:49 +0000

    sum all numbers in array test passed

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+
 function sumItems(array) {
   // Sum all the numbers in the array
+  var sum = 0;
+   array.forEach(function(num){
+   	
+    if (Array.isArray(num)) {
+      sum += sumItems(num); 
+    } else{
+	   sum += num;
+    }
+  });
+  return sum;
 }
+
 
 module.exports = sumItems;
